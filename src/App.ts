f7f43d46e6108a21b3036b91951e2a069169688f
renatus940804 <wonjun.s.dev@gmail.com>
2026-07-14 14:33:16 +0900

    feat(header): 헤더 구성 및 스타일링 추가

--- src/App.ts
+++ src/App.ts
@@ -1,14 +1,16 @@
 import { Component } from './core/core';
+import { TheHeader } from './components/TheHeader';
 
 export default class App extends Component {
   render() {
     // HEADER
+    const theHeader = new TheHeader().el;
 
     // ROUTER
     const routerView = document.createElement('router-view');
 
     // FOOTER
 
-    this.el.append(routerView);
+    this.el.append(theHeader, routerView);
   }
 }
---
+++ src/components/TheHeader.ts
@@ -0,0 +1,55 @@
+import { Component } from '../core/core';
+
+interface State {
+  [key: string]: unknown;
+  menus: {
+    name: string;
+    href: string;
+  }[];
+}
+
+export class TheHeader extends Component {
+  public state!: State;
+  constructor() {
+    super({
+      tagName: 'header',
+      state: {
+        menus: [
+          { name: 'Search', href: '#/' },
+          { name: 'Movie', href: '#/movie?id=tt4520988' },
+          { name: 'About', href: '#/about' },
+        ],
+      },
+    });
+
+    window.addEventListener('popstate', () => {
+      this.render();
+    });
+  }
+
+  render() {
+    this.el.innerHTML = `
+      <a href='#/' class="logo"><span>OMDbAPI</span>.COM</a>
+      <nav>
+        <ul>
+          ${this.state.menus
+            .map((menu) => {
+              const href = menu.href.split('?')[0];
+              const hash = location.hash.split('?')[0];
+              const isActive = href === hash;
+
+              return `
+            <li>
+              <a class="${isActive ? 'active' : ''}" href="${menu.href}">${menu.name}</a>
+            </li>
+            `;
+            })
+            .join('')}
+        </ul>
+      </nav>
+      <a href='#/about' class="user">
+        <img src="https://heropy.blog/css/images/logo.png" alt="User" />
+      </a>
+    `;
+  }
+}
