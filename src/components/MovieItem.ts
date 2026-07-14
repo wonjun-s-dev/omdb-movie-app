38d18b3c83ee6e4fd39104167cb56fa7718a43f1
renatus940804 <wonjun.s.dev@gmail.com>
2026-07-14 16:58:28 +0900

    feat(routing-home): MovieItem, MovieList 구성요소 및 구독로직 추가

---
+++ src/components/MovieItem.ts
@@ -0,0 +1,32 @@
+import { Component } from '../core/core';
+import type { SimpleMovie } from '../store/movie';
+
+interface Props {
+  [key: string]: unknown;
+  movie: SimpleMovie;
+}
+
+export default class MovieItem extends Component {
+  public props!: Props;
+
+  constructor(props: Props) {
+    super({
+      props,
+      tagName: 'a',
+    });
+  }
+
+  render() {
+    const { movie } = this.props;
+
+    this.el.setAttribute('href', `#/movie?id=${movie.imdbID}`);
+    this.el.classList.add('movie');
+    this.el.style.backgroundImage = `url(${movie.Poster})`;
+    this.el.innerHTML = `
+      <div class="info">
+        <div class="year">${movie.Year}</div>
+        <div class="title">${movie.Title}</div>
+      </div>
+    `;
+  }
+}
